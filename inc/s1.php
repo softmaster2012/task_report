1790c99aa555f00f4efb5b5d7a787eeea96cc81d
Nerguth <Nerguth@192.168.2.102>
2015-05-09 15:21:57 +0300

    Оперативные изменения: 09.05.2015 15:22

--- inc/s1.php
+++ inc/s1.php
@@ -1,1 +1,7 @@
 <h4>Решение 1</h4>
+<p style="text-align: left; text-indent: 20px">
+    Первоисточник: &reg;
+    <a href="http://www.captcha.ru/kcaptcha/" target="_blank">
+        http://www.captcha.ru/kcaptcha/
+    </a>
+</p>
